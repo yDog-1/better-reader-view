13c4c0e5c7620decd0eda937a36a68281fe42d82
yDog <ydog-1@outlook.jp>
2025-05-31 13:53:56 +0900

    feat(config): add permissions
    add permissions to wxt.config.ts
    - "activeTab" permission to allow the extension to interact with the active tab
    - "scripting" permission to allow the extension to inject scripts into web pages

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
 	manifest: {
+		permissions: ["activeTab","scripting"],
 		action: {},
 	},
 	modules: ["@wxt-dev/module-react"],
