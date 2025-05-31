bcde8aa757dd1b09bb90649b1c60443247afe956
yDog <ydog-1@outlook.jp>
2025-05-31 13:55:30 +0900

    feat(background): define browser action onclick listener
    Add a onclick listener for the browser action button in background.ts.
    It runs the content script.

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,5 +1,12 @@
 export default defineBackground(() => {
-	(browser.action ?? browser.browserAction).onClicked.addListener(() => {
-		console.log("Action button clicked");
-	});
+	(browser.action ?? browser.browserAction).onClicked.addListener(
+		async (tab) => {
+			if (tab.id && tab.url) {
+				await browser.scripting.executeScript({
+					target: { tabId: tab.id },
+					files: ["/content-scripts/content.js"],
+				});
+			}
+		},
+	);
 });
